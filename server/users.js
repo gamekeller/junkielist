58090bc65ed10b5156fd76c690ce79921e5aba2c
Heinrich Fenkart <hnrch02@gmail.com>
2013-09-27 00:24:48 +0200

    Removes verification system, closes #11

--- server/users.js
+++ server/users.js
@@ -1,14 +1,10 @@
 Accounts.onCreateUser(function(options, user) {
   user.profile = options.profile || {};
   user.shows = {};
-  // users start pending and need to be invited
-  user.isVerified = false;
 
-  // if this is the first user ever, make them an admin and verify them
-  if(!Meteor.users.find().count()) {
+  // if this is the first user ever, make them an admin
+  if(!Meteor.users.find().count())
     user.isAdmin = true;
-    user.isVerified = true;
-  }
 
   return user;
 });
